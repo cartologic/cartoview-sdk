c6d42af6fb586c94cce3cc62f591f67eb99be6a6
hisham waleed karam <hisham.karam@cartologic.com>
2017-06-16 03:32:17 +0200

    release 0.5.2

--- src/services/Assemblers.jsx
+++ src/services/Assemblers.jsx
@@ -10,7 +10,7 @@ export class RestAssembler {
     return this.api.segment(addSlash(url)+':id').param('id',id).query(filters).toString()
   }
   listUrl(url){
-    return this.api.segment(addSlash(url)').toString()
+    return this.api.segment(addSlash(url)).toString()
   }
 
 }
