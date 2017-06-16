3c0376af916503883f99102e6d85db06cd054746
hisham waleed karam <hisham.karam@cartologic.com>
2017-06-16 03:27:27 +0200

    Fix not a function bug

--- src/services/Assemblers.jsx
+++ src/services/Assemblers.jsx
@@ -1,7 +1,7 @@
 import SafeUrlAssembler from 'safe-url-assembler';
 import {hasTrailingSlash,addSlash} from '../helpers/helpers';
 
-export default class RestAssembler {
+export class RestAssembler {
   constructor(apiBaseUrl){
     this.api=SafeUrlAssembler(addSlash(apiBaseUrl) );
   }
@@ -10,7 +10,7 @@ export default class RestAssembler {
     return this.api.segment(addSlash(url)+':id').param('id',id).query(filters).toString()
   }
   listUrl(url){
-    return this.api.segment(addSlash(url)+':id').toString()
+    return this.api.segment(addSlash(url)').toString()
   }
 
 }
