2191ff6ebbd7c265b0a0a7655df8cc7baecd6151
hisham waleed karam <hisham.karam@cartologic.com>
2018-03-21 15:17:27 +0200

    handle geoserver exceptions

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,34 +1,55 @@
 import { getCRSFToken } from '../helpers/helpers'
 
-export const doGet = ( url, extraHeaders = {} ) => {
-    return fetch( url, {
+export const doGet = (url, extraHeaders = {}, type = 'json') => {
+    return fetch(url, {
         method: 'GET',
         credentials: 'include',
         headers: {
             "X-CSRFToken": getCRSFToken(),
             ...extraHeaders
         }
-    } ).then( ( response ) => {
-        return response.json()
-    } )
+    }).then((response) => {
+        try {
+            if (type === 'json') {
+                return response.json()
+            } else if (type === 'xml') {
+                return response.text()
+            }
+        } catch (err) {
+            console.error(url, err.message)
+            if (!response.ok) {
+                console.error(url, response.status)
+            }
+            return response.text().then(errMsg => { throw errMsg })
+        }
+    })
 }
-export const capitalizeFirstLetter = ( string ) => {
-    return string.charAt( 0 ).toUpperCase() + string.slice( 1 );
+export const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1)
 }
-export const doPost = ( url, data, extraHeaders = {}, type = 'json' ) => {
-    return fetch( url, {
+export const doPost = (url, data, extraHeaders = {}, type = 'json') => {
+    return fetch(url, {
         method: 'POST',
         credentials: 'include',
-        headers: new Headers( {
+        headers: new Headers({
             "X-CSRFToken": getCRSFToken(),
             ...extraHeaders
-        } ),
+        }),
         body: data
-    } ).then( ( response ) => {
-        if ( type === 'json' ) {
-            return response.json()
-        } else if ( type === 'xml' ) {
-            return response.text()
+    }).then((response) => {
+        try {
+            if (type === 'json') {
+                return response.json()
+            } else if (type === 'xml') {
+                return response.text()
+            }
+        } catch (err) {
+            console.error(url, err.message)
+            if (!response.ok) {
+                console.error(url, response.status)
+            }
+            return response.text().then(errMsg => { throw errMsg })
         }
-    } )
+
+    })
 }
