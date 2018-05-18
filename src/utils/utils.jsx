f2170393fe1a3425fa5d5389906c522dc4905db9
hisham waleed karam <hisham.karam@cartologic.com>
2018-05-18 04:06:17 +0200

    add utils docs

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,7 +1,13 @@
 import FileSaver from 'file-saver'
 import copy from 'clipboard-copy'
 import { getCRSFToken } from '../helpers/helpers'
-
+/**
+ * send get Request to an URL 
+ * @param {string} url to send request to
+ * @param {object} extraHeaders custom headers to add to the request
+ * @param {string} type expected response type to parse
+ * @returns {Promise} result
+ */
 export const doGet = (url, extraHeaders = {}, type = 'json') => {
     return fetch(url, {
         method: 'GET',
@@ -18,6 +24,13 @@ export const doGet = (url, extraHeaders = {}, type = 'json') => {
         }
     })
 }
+/**
+ * send get Request to an External URL (i.e not on the same domain)
+ * @param {string} url to send request to
+ * @param {object} extraHeaders custom headers to add to the request
+ * @param {string} type expected response type to parse
+ * @returns {Promise} result
+ */
 export const doExternalGet = (url, extraHeaders = {}, type = 'json') => {
     return fetch(url, {
         method: 'GET',
@@ -33,9 +46,21 @@ export const doExternalGet = (url, extraHeaders = {}, type = 'json') => {
         }
     })
 }
+/**
+ * capitalize First Letter of string
+ * @param {string} string the word to capitalize
+ * @returns {string} capitalized word
+ */
 export const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1)
 }
+/**
+ * send post Request to an URL 
+ * @param {string} url to send request to
+ * @param {object} extraHeaders custom headers to add to the request
+ * @param {string} type expected response type to parse
+ * @returns {Promise} result
+ */
 export const doPost = (url, data, extraHeaders = {}, type = 'json') => {
     return fetch(url, {
         method: 'POST',
@@ -54,6 +79,13 @@ export const doPost = (url, data, extraHeaders = {}, type = 'json') => {
         }
     })
 }
+/**
+ * Download binary data as file from the server
+ * @param {string} url to send request to
+ * @param {string} fileName the desired name of the file
+ * @param {string} data [data=null] - request body if you want to send post request
+ * @returns {void}
+ */
 export const downloadFile = (url, fileName, data = null) => {
     let mainProps = { method: 'GET' }
     if (data) {
@@ -72,6 +104,11 @@ export const downloadFile = (url, fileName, data = null) => {
         FileSaver.saveAs(data, fileName)
     }))
 }
+/**
+ * Copy data to system Clipboard
+ * @param {string} text text you want to copy
+ * @returns {Promise}
+ */
 export const copyToClipboard = (text = '') => {
     return copy(text)
 }
