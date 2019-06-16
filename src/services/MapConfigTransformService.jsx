8d505322d6e184b0601f27811b8af3bff94fac30
hisham waleed karam <hisham.karam@cartologic.com>
2019-06-16 17:19:57 +0200

    Revert "fix mapconfig service "
    This reverts commit e30d400c0f2ccc73784acde8a06cfad58993f7e0.

--- src/services/MapConfigTransformService.jsx
+++ src/services/MapConfigTransformService.jsx
@@ -205,21 +205,6 @@ class MapConfigTransformService {
         for (i = 0, ii = data.map.layers.length; i < ii; ++i) {
             var layer = data.map.layers[i];
             var source = data.sources[layer.source];
-            var sourceType = source.ptype;
-            if (Object.keys(source).length == 0 && layer.type === "osm") {
-                sourceType = "gxp_osmsource";
-                source.ptype = sourceType;
-            } else if (layer.provider && layer.provider === "OpenTopoMap") {
-                layer.type = 'OpenLayers.Layer.XYZ';
-                sourceType = "gxp_olsource";
-                source.ptype = "gxp_olsource";
-            } else if (Object.keys(source).length === 0 && layer.type === "empty") {
-                sourceType = "gxp_olsource";
-                source.ptype = "gxp_olsource";
-            }
-            if (!sourceType) {
-                sourceType = data.defaultSourceType;
-            }
             var url = source.url;
             var layerConfig = {
                 properties: {
@@ -231,19 +216,14 @@ class MapConfigTransformService {
                     name: layer.name
                 }
             };
-            if (sourceType === 'gxp_olsource' && layer.type ===
+            if (source.ptype === 'gxp_olsource' && layer.type ===
                 'OpenLayers.Layer.XYZ') {
                 layerConfig.type = 'Tile';
-                layerConfig.properties.title = layer.args && layer.args.length > 0 ? layer.args[0] : layerConfig.properties.title;
+                layerConfig.properties.title = layer.args[0];
                 layerConfig.properties.name = layerConfig.properties.title
                     .split(' ').join('_');
                 var xyzUrls;
-                var urlConfig = layer.args && layer.args.lenght ? layer.args[1] : undefined;
-                if (layer.provider && layer.provider === "OpenTopoMap") {
-                    urlConfig = ['https://a.tile.opentopomap.org/{z}/{x}/{y}.png',
-                        'https://b.tile.opentopomap.org/{z}/{x}/{y}.png',
-                        'https://c.tile.opentopomap.org/{z}/{x}/{y}.png'];
-                }
+                var urlConfig = layer.args[1];
                 if (Array.isArray(urlConfig)) {
                     xyzUrls = urlConfig;
                 } else {
@@ -263,11 +243,11 @@ class MapConfigTransformService {
                         urls: xyzUrls
                     }
                 };
-                if (layer.args && layer.args.length === 3 && layer.args[2].attribution) {
+                if (layer.args.length === 3 && layer.args[2].attribution) {
                     layerConfig.source.properties.attributions = [layer.args[
                         2].attribution];
                 }
-            } else if (sourceType === 'gxp_osmsource') {
+            } else if (source.ptype === 'gxp_osmsource') {
                 if (!layer.group) {
                     // force OSM as base layer
                     layerConfig.properties.type = 'base';
@@ -279,7 +259,7 @@ class MapConfigTransformService {
                         crossOrigin: 'anonymous'
                     }
                 };
-            } else if (sourceType === 'gxp_arcrestsource') {
+            } else if (source.ptype === 'gxp_arcrestsource') {
                 layerConfig.type = 'Tile';
                 layerConfig.source = {
                     type: 'TileArcGISRest',
@@ -292,7 +272,7 @@ class MapConfigTransformService {
                         }
                     }
                 };
-            } else if (sourceType === 'gxp_wmscsource' && layer.name) {
+            } else if (source.ptype === 'gxp_wmscsource' && layer.name) {
                 layerConfig.properties.popupInfo = '#AllAttributes';
                 layerConfig.properties.isSelectable = layer.queryable;
                 layerConfig.properties.isWFST = layer.queryable;
@@ -351,12 +331,15 @@ class MapConfigTransformService {
                     delete layerConfig.source.urls
                     layerConfig.source.properties.url = url
                     layerConfig.source.properties.hidpi = false
-                    if (layer.capability) {
-                        layerConfig.properties.extent = layer.capability.bbox[data.map.projection].bbox;
-                    }
+                    layerConfig.properties.extent = layer.capability.bbox[data.map.projection].bbox
+                    // if (layer.srs === data.map.projection) {
+
+                    // } else {
+                    //     layerConfig.properties.extent = proj.transformExtent(layer.bbox, layer.srs, data.map.projection)
+                    // }
 
                 };
-            } else if (sourceType === 'gxp_mapboxsource') {
+            } else if (source.ptype === 'gxp_mapboxsource') {
                 var urls = [
                     'http://a.tiles.mapbox.com/v1/mapbox.' + layer.name + '/',
                     'http://b.tiles.mapbox.com/v1/mapbox.' + layer.name + '/',
@@ -392,22 +375,13 @@ class MapConfigTransformService {
                         maxZoom: maxZoom[layer.name]
                     }
                 };
-            } else if (sourceType === 'gxp_bingsource') {
+            } else if (source.ptype === 'gxp_bingsource') {
                 layerConfig.type = 'Tile';
                 layerConfig.source = {
                     type: 'BingMaps',
                     properties: {
                         key: source.apiKey,
                         imagerySet: layer.name
-                    }
-                };
-            } else if (sourceType === "gxp_olsource") {
-                layerConfig.type = 'Tile';
-                layerConfig.source = {
-                    type: 'XYZ',
-                    properties: {
-                        crossOrigin: crossOrigin,
-                        urls: ['']
                     }
                 };
             } else {
